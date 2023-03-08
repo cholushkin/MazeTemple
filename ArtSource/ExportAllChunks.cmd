4b659891da298837789178cce1f4c0c7fa97791f
max <cholushkin@gmail.com>
2023-03-08 22:25:26 +0200

    + Art sources added

---
+++ ArtSource/ExportAllChunks.cmd
@@ -0,0 +1,19 @@
+cd ExportScripts
+call config.cmd
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk FBX/PlatformChunk.000.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.001 FBX/PlatformChunk.001.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.002 FBX/PlatformChunk.002.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.003 FBX/PlatformChunk.003.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.004 FBX/PlatformChunk.004.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.005 FBX/PlatformChunk.005.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.006 FBX/PlatformChunk.006.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.007 FBX/PlatformChunk.007.fbx
+call export_fbx_object.cmd ../Chunks.blend PlatformChunk.008 FBX/PlatformChunk.008.fbx
+call export_fbx_object.cmd ../Chunks.blend BridgeSegment FBX/BridgeSegment.fbx
+call export_fbx_object.cmd ../Chunks.blend Pedestal FBX/Pedestal.fbx
+call export_fbx_object.cmd ../Chunks.blend TowerChunk FBX/TowerChunk.000.fbx
+call export_fbx_object.cmd ../Chunks.blend TowerChunk.001 FBX/TowerChunk.001.fbx
+call export_fbx_object.cmd ../Chunks.blend TowerChunk.002 FBX/TowerChunk.002.fbx
+call export_fbx_object.cmd ../Chunks.blend TowerChunk.003 FBX/TowerChunk.003.fbx
+call export_fbx_object.cmd ../Chunks.blend TowerChunk.004 FBX/TowerChunk.004.fbx
+call export_fbx_object.cmd ../Chunks.blend TowerChunk.005 FBX/TowerChunk.005.fbx
